673942b1fb304882c5ababb59d10a76ae5d806bb
André Freitas <afelixfreitas@gmail.com>
2023-09-25 08:34:44 -0300

    style: remove console.log

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 export function getAPIClient() {
   const baseURL = process.env.REACT_APP_BASE_URL;
-
-  console.log(baseURL, "baseURL");
-
   const api = axios.create({ baseURL });
 
   return api;
